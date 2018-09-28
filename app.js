9bbbbe58d6a20fb31ba16aac68094a8e273b1576
Carl Hüffmeier <carlhueffmeier@gmail.com>
2018-09-28 10:52:35 +0200

    refactor: Remove unused package

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
-const serve = require('koa-static');
 const router = require('./router.js');
 const bodyParser = require('koa-body-parser');
 const cors = require('koa-cors');
 require('dotenv').config();
 
 const options = {
-  origin: process.env.CLIENT_URL,
-}
+  origin: process.env.CLIENT_URL
+};
 
-app.use(bodyParser())
+app
+  .use(bodyParser())
   .use(cors(options))
   .use(router.routes())
-  .use(router.allowedMethods())
+  .use(router.allowedMethods());
 
-  
 app.listen(process.env.PORT || 5555);
