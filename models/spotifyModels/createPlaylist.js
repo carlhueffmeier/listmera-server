548affdf561300f3cdf91ab7c4326e19f7e1bd58
Carl Hüffmeier <carlhueffmeier@gmail.com>
2018-10-01 12:25:29 +0200

    refactor: Split createPlaylist into smaller chunks

--- models/spotifyModels/createPlaylist.js
+++ models/spotifyModels/createPlaylist.js
@@ -2,157 +2,126 @@ const spotify = require('../../secrets/spotifyConf.js');
 const removeAdmin = require('../userModels/removeAdmin.js');
 const expire = require('../playlistModels/setExpiry.js');
 
-async function generatePlaylist(playlist, refresh, identifier, copy, copier) {
-  let playlistId;
-  let tracks = playlist.tracks.map(el => `spotify:track:${el}`);
+// Converts a Spotify Track ID to a Spotify Track URI
+function createTrackUri(trackId) {
+  return 'spotify:track:' + trackId;
+}
+
+async function resetSpotifyAccessToken(refresh) {
   await spotify.setRefreshToken(refresh);
-  await spotify
-    .refreshAccessToken()
-    .then(async data => {
-      await spotify.setAccessToken(data.body['access_token']);
-    })
-    .catch(e => console.error(e));
+  const data = await spotify.refreshAccessToken();
+  spotify.setAccessToken(data.body['access_token']);
+}
+
+async function generatePlaylistCopy({ playlist, refresh, copier }) {
+  let trackUris = playlist.tracks.map(createTrackUri);
+  await resetSpotifyAccessToken(refresh);
+
   if (!playlist.strict) {
-    let seed;
-    let attributes;
-    if (playlist.tracks.length <= 5) {
-      seed = playlist.tracks;
-    } else {
-      seed = playlist.tracks.slice(0, 5);
-    }
-    const keys = [
-      'dance',
-      'energy',
-      'loud',
-      'instrumental',
-      'live',
-      'mood',
-      'major',
-      'minor',
-      'tempo'
-    ];
-    let flag = false;
-    attributes = Object.keys(playlist)
-      .filter(el => ~keys.indexOf(el))
-      .reduce(
-        (acc, el) => {
-          if (el === 'dance') {
-            return {
-              ...acc,
-              [`min_danceability`]: Number(playlist[el])
-            };
-          }
-          if (el === 'energy') {
-            return {
-              ...acc,
-              [`min_energy`]: Number(playlist[el])
-            };
-          }
-          if (el === 'instrumental') {
-            return {
-              ...acc,
-              [`min_instrumentalness`]: Number(playlist[el])
-            };
-          }
-          if (el === 'live') {
-            return {
-              ...acc,
-              [`min_liveness`]: Number(playlist[el])
-            };
-          }
-          if (el === 'loud') {
-            return {
-              ...acc,
-              [`max_loudness`]: Number(playlist[el])
-            };
-          }
-          if (el === 'tempo') {
-            return {
-              ...acc,
-              [`target_tempo`]: Number(playlist[el])
-            };
-          }
-          if (el === 'mood') {
-            return playlist[el] === 0
-              ? {
-                  ...acc,
-                  [`max_valence`]: 0.5
-                }
-              : {
-                  ...acc,
-                  [`min_valence`]: 0.5
-                };
-          }
-          if (el === 'minor') {
-            if (flag) {
-              return {
-                ...acc,
-                [`target_mode`]: false
-              };
-            } else {
-              flag = true;
-              return {
-                ...acc,
-                [`target_mode`]: 0
-              };
-            }
-          }
-          if (el === 'major') {
-            if (flag) {
-              return {
-                ...acc,
-                [`target_mode`]: false
-              };
-            } else {
-              flag = true;
-              return {
-                ...acc,
-                [`target_mode`]: 1
-              };
-            }
-          }
-        },
-        {
-          limit: 50 - playlist.tracks.length,
-          seed_tracks: seed
-        }
-      );
-    let recommended = await spotify
-      .getRecommendations(attributes)
-      .then(res => res.body.tracks.map(el => `spotify:track:${el.id}`));
-    tracks = tracks.concat(recommended);
+    trackUris = [...trackUris, await getTrackRecommendations(playlist, playlist.tracks)];
   }
-  if (!copy) {
-    await spotify
-      .createPlaylist(playlist.adminId, playlist.name, { description: 'powered by listmera' })
-      .then(res => {
-        playlistId = res.body.id;
-      })
-      .catch(e => console.error(e));
-    await spotify
-      .addTracksToPlaylist(playlist.adminId, playlistId, tracks)
-      .catch(e => console.error(e));
-    await removeAdmin({
-      username: playlist.adminId,
-      id: identifier
-    });
-    await expire({
-      playlist: identifier,
-      bank: playlist.bank,
-      tracks: playlist.trackId,
-      collabs: playlist.collabs
-    });
-  } else {
-    await spotify
-      .createPlaylist(copier.username, playlist.name, { description: 'powered by listmera' })
-      .then(res => {
-        playlistId = res.body.id;
-      })
-      .catch(e => console.error(e));
-    await spotify
-      .addTracksToPlaylist(copier.username, playlistId, tracks)
-      .catch(e => console.error(e));
+  const createPlaylistOptions = [
+    copier.username,
+    playlist.name,
+    { description: 'powered by listmera' }
+  ];
+  const result = await spotify.createPlaylist(...createPlaylistOptions);
+  const copiedPlaylistId = result.body.id;
+  await spotify.addTracksToPlaylist(copier.username, copiedPlaylistId, trackUris);
+}
+
+async function generatePlaylist({ playlist, refresh, id }) {
+  let trackUris = playlist.tracks.map(createTrackUri);
+  await resetSpotifyAccessToken(refresh);
+
+  if (!playlist.strict) {
+    trackUris = [...trackUris, await getTrackRecommendations(playlist, playlist.tracks)];
+  }
+  const createPlaylistOptions = [
+    playlist.adminId,
+    playlist.name,
+    { description: 'powered by listmera' }
+  ];
+  const result = await spotify.createPlaylist(...createPlaylistOptions);
+  const newPlaylistId = result.body.id;
+  await spotify.addTracksToPlaylist(playlist.adminId, newPlaylistId, trackUris);
+  await removeAdmin({
+    username: playlist.adminId,
+    id
+  });
+  await expire({
+    playlist: id,
+    bank: playlist.bank,
+    tracks: playlist.trackId,
+    collabs: playlist.collabs
+  });
+}
+
+async function getTrackRecommendations(playlist, seedTracks) {
+  if (!seedTracks || seedTracks.length === 0) {
+    throw new Error('You need to specify at least one track to generate recommendations');
+  }
+  if (seedTracks.length >= 5) {
+    seedTracks = seedTracks.slice(0, 5);
+  }
+  const attributes = generateAttributes(playlist, seedTracks);
+  const response = await spotify.getRecommendations(attributes);
+  const recommendedTracks = response.body.tracks.map(track => createTrackUri(track.id));
+  return recommendedTracks;
+}
+
+function generateAttributes(playlist, seedTracks) {
+  const validAttributes = [
+    'dance',
+    'energy',
+    'loud',
+    'instrumental',
+    'live',
+    'mood',
+    'major',
+    'minor',
+    'tempo'
+  ];
+  const attributeDictionary = {
+    dance: 'min_danceability',
+    energy: 'min_energy',
+    instrumental: 'min_instrumentalness',
+    live: 'min_liveness',
+    loud: 'max_loudness',
+    tempo: 'target_tempo'
+  };
+  const initialAttributes = {
+    limit: 50 - seedTracks.length,
+    seed_tracks: seedTracks
+  };
+
+  return Object.entries(playlist)
+    .filter(attribute => validAttributes.includes(attribute))
+    .reduce(attributeReducer, initialAttributes);
+
+  function attributeReducer(attributes, [currentAttribute, value]) {
+    if (attributeDictionary.includes(currentAttribute)) {
+      let key = attributeDictionary[currentAttribute];
+      attributes[key] = value;
+    } else if (currentAttribute === 'mood' && value === 0) {
+      attributes.max_valence = 0.5;
+    } else if (currentAttribute === 'mood' && value === 1) {
+      attributes.min_valence = 0.5;
+    } else if (currentAttribute === 'minor') {
+      attributes.target_mode = 0;
+    } else if (currentAttribute === 'major') {
+      attributes.target_mode = 1;
+    }
+    return attributes;
   }
 }
 
-module.exports = generatePlaylist;
+function compatGeneratePlaylist(playlist, refresh, id, copy, copier) {
+  if (copy) {
+    return generatePlaylistCopy({ playlist, refresh, copier });
+  } else {
+    return generatePlaylist({ playlist, refresh, id });
+  }
+}
+
+module.exports = compatGeneratePlaylist;
